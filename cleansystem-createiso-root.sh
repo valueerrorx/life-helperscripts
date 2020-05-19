75341ec34bedafc34abd9f0ba1cfe7e81be6f5d1
valueerror <valueerror@gmail.com>
2020-05-19 11:36:45 +0200

    bleachbit

--- cleansystem-createiso-root.sh
+++ cleansystem-createiso-root.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 clear
 
-
-
 USER=$(logname)
 
 echo "----------------------------- "
@@ -14,7 +12,6 @@ sudo apt-get autoclean
 sudo apt-get autoremove
 
 
-
 echo "copy firststartwizard to autostart folder"
 cp /home/student/.life/applications/life-firststart/firststart.sh /home/student/.config/autostart-scripts/
 
@@ -23,8 +20,16 @@ echo "removing cache files"
 sudo rm /var/tmp/kdecache-root/*  > /dev/null 2>&1
 sudo rm /var/tmp/kdecache-student/* > /dev/null 2>&1
 sudo rm /var/crash/* > /dev/null 2>&1
+
+sudo rm /var/lib/snapd/cache/ -r > /dev/null 2>&1
+
+
 rm /home/student/.xsession-errors
+
+echo "removing log files"
 rm /home/student/.life/EXAM/client.log > /dev/null 2>&1
+find /home/student/.life/applications/life-exam/ -type f -name "*.log" -exec rm -f {} \;  > /dev/null 2>&1
+
 rm /home/student/.local/share/RecentDocuments/*  > /dev/null 2>&1
 rm /home/student/.kde/share/apps/RecentDocuments/*  > /dev/null 2>&1
 
@@ -42,13 +47,19 @@ rm -r /home/student/.life/EXAM/ > /dev/null 2>&1
 echo "cleaning bash history"
 history -w
 history -c
-rm /home/student/.bash_history
-
+rm /home/student/.bash_history > /dev/null 2>&1
 
 
 echo "starting bleachbit as root and as user"
+#cleaning all necessary stuff
 sudo bleachbit > /dev/null 2>&1
-sudo -u ${USER} -H bleachbit
+
+LIST='adobe_reader|amsn|amule|apt|audacious|bash|d4x|epiphany|evolution|filezilla|flash|gwenview|journald|kde|libreoffice|liferea|midnightcommander|nautilus|openofficeorg|opera|thunderbird|x11|yum'
+SLIST='system.trash|system.cache|system.clipboard|system.recent_documents|system.rotated_logs|system.tmp'
+bleachbit --list | grep -E "[a-z0-9_\-]+\.[a-z0-9_\-]+" | grep -E ${LIST} | xargs bleachbit --clean > /dev/null 2>&1
+bleachbit --list | grep -E "[a-z0-9_\-]+\.[a-z0-9_\-]+" | grep -E ${SLIST} | xargs bleachbit --clean
+sudo -u ${USER} -H bleachbit --list | grep -E "[a-z0-9_\-]+\.[a-z0-9_\-]+" | grep -E ${LIST} | xargs bleachbit --clean > /dev/null 2>&1
+
 echo "clear bash history"
 history -w
 history -c
