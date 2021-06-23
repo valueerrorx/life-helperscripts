9385614c41ec0dbca6bee8584df07b1729b1751c
valueerror <valueerror@gmail.com>
2021-06-24 00:58:32 +0200

    change chrome prefs

--- chrome-cookies-autodelete.sh
+++ chrome-cookies-autodelete.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-cd ~/.config/google-chrome/
+cd ~/.config/google-chrome/Default
 
-sed -i 's/"exit_type": ".*"/"exit_type": "Normal","default_content_setting_values": { "cookies": 4 }/g' Preferences
+cat Preferences | jq . > Preferences1
+
+sleep 1
+
+sed -i 's/"exit_type": "Crashed"/"exit_type": "Normal"/g' Preferences1 
+
+sed -i 's/"exit_type": "Normal"/"exit_type": "Normal","default_content_setting_values": { "cookies": 4 }/g' Preferences1
+
+cat Preferences1 > Preferences
