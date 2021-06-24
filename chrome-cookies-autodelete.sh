201d9d8076f16bc404fc113a23da42ce874472cb
valueerror <valueerror@gmail.com>
2021-06-24 14:55:41 +0200

    added firefox clear cookies

--- chrome-cookies-autodelete.sh
+++ chrome-cookies-autodelete.sh
@@ -11,3 +11,14 @@ sed -i 's/"exit_type": "Crashed"/"exit_type": "Normal"/g' Preferences1
 sed -i 's/"exit_type": "Normal"/"exit_type": "Normal","default_content_setting_values": { "cookies": 4 }/g' Preferences1
 
 cat Preferences1 > Preferences
+
+
+
+#for firefox
+cd ~/.mozilla/firefox
+PREFSPATH=$(find . |grep "prefs.js" )
+
+while IFS= read -r line; do     
+    echo 'user_pref("network.cookie.lifetimePolicy", 2);' >>  $line
+done <<< "$PREFSPATH"
+
