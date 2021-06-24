ee14d179bb12209a09c9adb02d41fa7021de5ff6
Your Name <you@example.com>
2021-06-24 08:55:47 +0200

    fixed chrome cookies

--- chrome-cookies-autodelete.sh
+++ chrome-cookies-autodelete.sh
@@ -11,3 +11,6 @@ sed -i 's/"exit_type": "Crashed"/"exit_type": "Normal"/g' Preferences1
 sed -i 's/"exit_type": "Normal"/"exit_type": "Normal","default_content_setting_values": { "cookies": 4 }/g' Preferences1
 
 cat Preferences1 > Preferences
+
+
+exit 0
